99707a72c1f7a265c2a66a38e864fcc2992aa6e6
rashedul24 <ronyuap@gmail.com>
2025-05-26 15:29:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar/navbar'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Navbar></Navbar>
+      </div>
+    </>
+  )
+}
+
+export default App
